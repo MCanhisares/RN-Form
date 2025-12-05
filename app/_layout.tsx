130de8bddf0495adee4b39f4e2c7f193293b07b5
Marcel Canhisares <marcel.canhisares@gmail.com>
2025-12-05 00:36:30 -0500

    Adding i18n file and toggle options

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
+import { LanguageToggle } from '@components/languageToggle/LanguageToggle';
 import { Stack } from 'expo-router';
+import './i18n/config';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerLeft: () => <LanguageToggle />,
+          headerStyle: {
+            backgroundColor: 'transparent',
+          },
+        }}
+      />
+    </Stack>
+  );
 }
